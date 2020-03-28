29332e7a9802cd29b3d8e842feaf3f4beaddf3dd
averu <neuf-seize@outlook.jp>
2020-03-29 06:17:59 +0900

    add: ベースファイルを追加

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<main>
+  <h1>Astrology</h1>
+</main>
+
+<style>
+  main {
+    margin: 0 auto;
+    background-color: #aaa;
+  }
+
+  h1 {
+    font-family: 'Fredoka One', cursive;
+    text-align: center;
+    color: #fff;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
---
+++ src/components/FontSetting.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<div class="panel">
+  <div class="">
+    フォント設定
+  </div>
+</div>
+
+<style>
+  .panel {
+    background-color: #5a5a5a;
+    width: 300px;
+
+  }
+</style>
