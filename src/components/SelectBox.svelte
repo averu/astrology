a818dc6c219ee7434e295add7763893e4adb7122
averu <neuf-seize@outlook.jp>
2020-03-29 12:27:46 +0900

    add: SelectBoxのテンプレート追加

---
+++ src/components/SelectBox.svelte
@@ -0,0 +1,22 @@
+<script>
+  const options = [
+    { name: '' },
+    { name: 'hoge1' },
+    { name: 'hoge2' },
+    { name: 'hoge3' },
+  ];
+  let selected = options[0];
+</script>
+
+<select bind:value="{selected}">
+  {#each options as option}
+    <option value="{option}">{option.name}</option>
+  {/each}
+</select>
+
+<style>
+  select {
+    width: 150px;
+    border-radius: 3px;
+  }
+</style>
